f80580b3f6bba2d8e4e3f4ce2cf4dab4621e43c5
Daniel <daniel.g.mason@gmail.com>
2024-03-21 16:58:23 -0700

    parsed the app out

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import os
-from dotenv import load_dotenv
 
-from pinecone import Pinecone
-from pinecone import PodSpec
-
-from langchain_pinecone import PineconeVectorStore
-from langchain_community.llms import Ollama
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
-
+#app wrapper
 import streamlit as st
+
+#not sure but maybe a best practice? i dunno what this does
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -21,33 +12,67 @@ load_dotenv()
 os.environ["GOOGLE_API_KEY"] = st.secrets.GOOGLE_API_KEY
 os.environ["PINECONE_API_KEY"] = st.secrets.PINECONE_API_KEY
 
-#creating a Pinecone client
-spec = PodSpec(environment="gcp-starter")
-pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
-
-#selecting the pinecone index
-index = pc.Index("portfolio")
-
 #setting the llm and embeddings
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+
 llm = Ollama(model="mixtral:8x7b")
 embeddings = OllamaEmbeddings(model="mixtral:8x7b")
 
-#creating the PineconeVectorStore
-vectorsstore = PineconeVectorStore(
-    index, embeddings
-    )
+#initializing Pinecone
+from pinecone import Pinecone, ServerlessSpec, PodSpec
+import time
 
-#dowloading the web contents
-loader = WebBaseLoader("https://portfolio.danielgmason.com")
-docs = loader.load()
-documents = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200
-).split_documents(docs)
+spec = PodSpec(environment="gcp-starter")
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
-#using the chunks of documents to create the index
-index = VectorStoreIndex.from_documents(documents)
+# configure client
+if use_serverless:  
+    spec = ServerlessSpec(cloud='aws', region='us-west-2')
+else:  
+    # if not using a starter index, you should specify a pod_type too  
+    spec = PodSpec()
+
+# check for and delete index if already exists  
+index_name = 'ux-for-ai'  
+if index_name in pc.list_indexes().names():  
+    pc.delete_index(index_name)  
+
+# create a new index  
+pc.create_index(  
+    index_name,  
+    dimension=1536,  # dimensionality of text-embedding-ada-002  
+    metric='dotproduct',  
+    spec=spec  
+)
+
+# wait for index to be initialized  
+while not pc.describe_index(index_name).status['ready']:  
+    time.sleep(1)
+
+pc_index = pc.Index(index_name)
+
+#generating the embeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+loader = TextLoader("./data/data.txt")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=512, chunk_overlap=200
+)
+docs = text_splitter.split_documents(documents)
+
+#adding the embeddings to the vector store
+from langchain_pinecone import PineconeVectorStore
+
+vectorstore = PineconeVectorStore(pc_index=pc_index, embeddings=embeddings)
+
+index = vectorstore.from_documents(docs)
 
 #making the prompts
+from langchain.prompts import PromptTemplate
+
 template = """
 Answer the question based on the context below. If you can't  answer the question, reply "This is a tricky one. I don't have an answer. Will you ask the question in another way."
 
@@ -58,7 +83,10 @@ Question: {question}
 prompt = PromptTemplate.from_template(template)
 
 # putting the chain together
-retriever = vectorstore.as_retriever()
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+retriever = index.as_retriever()
 chain = (
     {
         "context": retriever,
